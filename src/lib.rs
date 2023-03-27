6bea06251dc531f1da226c62fb931df7bc6162e2
Chris Hsu <chrisfishbob@gmail.com>
2023-03-26 22:55:22 -0700

    Added commandline args mode handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,54 @@
+use std::{error::Error, fmt};
+
+use html_escape::decode_html_entities_to_string;
 use reqwest::blocking::get;
 use scraper::{Html, Selector};
-use html_escape::decode_html_entities_to_string;
 
+// Custom error type
+#[derive(Debug)]
+struct BrothError(&'static str);
+
+impl fmt::Display for BrothError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(self.0)
+    }
+}
+
+impl std::error::Error for BrothError {}
+
+// Encapsulates the commandline args
+pub struct Command {
+    mode: String,
+    ticker: String,
+    _optional_flag: Option<String>,
+}
+
+impl Command {
+    pub fn build(args: &[String]) -> Result<Command, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let mode = args[1].clone();
+        let ticker = args[2].clone();
+
+        Ok(Command {
+            mode,
+            ticker,
+            _optional_flag: None,
+        })
+    }
+}
+
+pub fn run(command: Command) -> Result<(), Box<dyn Error>> {
+    let url = get_summary_url_from_ticker(&command.ticker);
+    let html = fetch_html(&url).unwrap();
+    match command.mode.to_lowercase().as_str() {
+        "quote" => println!("{}", extract_price(&html)?),
+        "fullname" => println!("{}", extract_full_name(&html)?),
+        _ => return Err(Box::new(BrothError("invalid mode"))),
+    }
+    Ok(())
+}
 
 pub fn fetch_html(url: &str) -> Result<scraper::html::Html, reqwest::Error> {
     let response = get(url)?;
@@ -10,21 +57,24 @@ pub fn fetch_html(url: &str) -> Result<scraper::html::Html, reqwest::Error> {
     Ok(html_object)
 }
 
-pub fn extract_price(html: &scraper::html::Html) -> String {
-    let div_selector = Selector::parse(r#"[data-test-id='symbol-price']"#).unwrap();
+pub fn extract_price(html: &scraper::html::Html) -> Result<String, Box<dyn Error>> {
+    let div_selector = Selector::parse(r#"[data-test-id='symbol-price']"#)?;
 
     let parsed_document: Vec<String> = html
         .select(&div_selector)
         .map(|element| element.inner_html())
         .collect();
     match parsed_document.len() {
-        1 => parsed_document.get(0).unwrap().replace("<!-- -->", ""),
-        _ => panic!("Parsing failed"),
+        1 => {
+            let price = parsed_document.get(0).unwrap().replace("<!-- -->", "");
+            Ok(price)
+        }
+        _ => Err(Box::new(BrothError("failed to extract price from ticker"))),
     }
 }
 
-pub fn extract_full_name(html: &scraper::html::Html) -> String {
-    let div_selector = Selector::parse(r#"[data-test-id='symbol-full-name']"#).unwrap();
+pub fn extract_full_name(html: &scraper::html::Html) -> Result<String, Box<dyn Error>> {
+    let div_selector = Selector::parse(r#"[data-test-id='symbol-full-name']"#)?;
 
     let parsed_document: Vec<String> = html
         .select(&div_selector)
@@ -35,9 +85,11 @@ pub fn extract_full_name(html: &scraper::html::Html) -> String {
             let name = parsed_document.get(0).unwrap().to_owned();
             let mut decoded_name = String::new();
             decode_html_entities_to_string(name, &mut decoded_name);
-            decoded_name
+            Ok(decoded_name)
         }
-        _ => panic!("Parsing failed"),
+        _ => Err(Box::new(BrothError(
+            "failed to extract full comany name from ticker",
+        ))),
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
-use std::env;
-use broth;
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Usage: {} <ticker>", args[0]);
+    let args: Vec<String> = std::env::args().collect();
+    let command = broth::Command::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         std::process::exit(1);
-    }
-    let ticker = &args[1];
-    let url = broth::get_summary_url_from_ticker(ticker);
+    });
 
-    match broth::fetch_html(&url) {
-        Ok(html) => {
-            let full_name = broth::extract_full_name(&html);
-            let price = broth::extract_price(&html);
-            println!("{full_name}");
-            println!("{price}");
-        }
-        Err(e) => eprintln!("Error fetching URL: {}", e),
-    }
+    if let Err(e) = broth::run(command) {
+        eprintln!("Broth error: {e}");
+        std::process::exit(1);
+    };
 }
